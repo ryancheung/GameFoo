1cf55476d6c5552dd7ac239e98ee7887c60b2ad0
ryancheung <ryancheung.go@gmail.com>
2020-12-11 21:32:40 +0800

    First

---
+++ Foo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Foo
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
